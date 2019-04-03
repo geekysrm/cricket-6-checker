40edfb860b9a15877a6dfa25e305e2b3a99627fd
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-04-04 00:56:08 +0530

    Fetch current over and current team batting

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ export async function getHTML(url) {
 export async function scrapeRuns(html) {
 	// load up cheerio
 	const $ = cheerio.load(html);
-	const span = $('[data-nav="followers"] .ProfileNav-value');
 	const scrapedRuns = $(".ui-branding-style-partner")
 		.children()
 		.children()
 		.children()
 		.children()[7].children[0].data;
-	console.log(scrapedRuns); //string
-	return scrapedRuns;
+	const currentStatus = $(".ui-bat-team-scores").text();
+	const currentTeam = currentStatus.split(" - ")[0];
+	const currentOver = currentStatus.match(/\((.*)\)/)[1];
+	// return scrapedRuns;
+	console.log(scrapedRuns);
+	console.log(currentTeam);
+	console.log(currentOver);
 }
 
 export async function getRuns() {
@@ -24,7 +28,8 @@ export async function getRuns() {
 		"https://m.cricbuzz.com/cricket-commentary/22410/mi-vs-csk-15th-match-indian-premier-league-2019"
 	);
 	const runs = await scrapeRuns(html);
-	return runs;
+
+	// return runs;
 }
 
 getRuns();
